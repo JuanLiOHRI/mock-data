70fbb10b79a047af7590752abcaba0e72872a36b
JuanLiOHRI <juli@ohri.ca>
2025-07-10 11:29:36 -0400

    using phiatyll

--- R/create_cat_var.R
+++ R/create_cat_var.R
@@ -4,6 +4,7 @@
 #' @param cycle string. The cycle to which the variable belongs (e.g., "CCHS2001").
 #' @param variable_details data.frame. A data frame containing variable details, typically loaded from a "variable-details" sheet.
 #' @param length integer. The desired length of the categorical variable vector.
+#' @param df_mock data frame, the current generated mock data, to check if the raw variable has already been created.
 #' @param prop_NA numeric. Optional. The proportion of NA values to be introduced. If NULL, no NA values are introduced.
 #' @param seed integer. Optional. Seed for reproducibility. Default is 100.
 #' @return A data frame with one column representing the newly created categorical variable.
@@ -12,7 +13,7 @@
 #' # create_cat_var("gender", "HC1", variable_details, 100)
 #' # create_cat_var("race", "HC1", variable_details, 100, prop_NA = 0.1)
 
-create_cat_var <- function(var, cycle, variable_details, length, prop_NA = NULL, seed = 100) {
+create_cat_var <- function(var, cycle, variable_details, length, df_mock, prop_NA = NULL, seed = 100) {
     # related rows in variable_details
     var_details <- variable_details[variable_details$variable == var & str_detect(variable_details$variableStart, cycle),]
 
@@ -21,45 +22,47 @@ create_cat_var <- function(var, cycle, variable_details, length, prop_NA = NULL,
         temp <- unlist(str_split(var_details$variableStart[1],","))
         var_raw <- unlist(str_split(temp[str_detect(temp, cycle)],"::"))[2]
 
-        # extract categories from `recStart` column
-        # real levels
-        labels <- var_details$recStart[!(str_detect(var_details$recEnd, "NA"))]
-        if (any(str_detect(labels, ","))) { # unpack the ranges
-            temp <- labels[!str_detect(labels, ",")]
-            ranges <- labels[str_detect(labels, ",")]
-            for (range in ranges) {
-                temp <- c(temp, unpack_range(range))
+        if (!(var_raw %in% names(df_mock))) {
+            # extract categories from `recStart` column
+            # real levels
+            labels <- var_details$recStart[!(str_detect(var_details$recEnd, "NA"))]
+            if (any(str_detect(labels, ","))) { # unpack the ranges
+                temp <- labels[!str_detect(labels, ",")]
+                ranges <- labels[str_detect(labels, ",")]
+                for (range in ranges) {
+                    temp <- c(temp, unpack_range(range))
+                }
+                labels <- sort(temp)
             }
-            labels <- sort(temp)
-        }
-        # NA levels
-        NA_labels <- var_details$recStart[str_detect(var_details$recEnd, "NA")]
-        if (any(str_detect(NA_labels, ","))) { # unpack the ranges
-            temp <- NA_labels[!str_detect(NA_labels, ",")]
-            ranges <- NA_labels[str_detect(NA_labels, ",")]
-            for (range in ranges) {
-                temp <- c(temp, unpack_range(range))
+            # NA levels
+            NA_labels <- var_details$recStart[str_detect(var_details$recEnd, "NA")]
+            if (any(str_detect(NA_labels, ","))) { # unpack the ranges
+                temp <- NA_labels[!str_detect(NA_labels, ",")]
+                ranges <- NA_labels[str_detect(NA_labels, ",")]
+                for (range in ranges) {
+                    temp <- c(temp, unpack_range(range))
+                }
+                NA_labels <- sort(temp)
             }
-            NA_labels <- sort(temp)
-        }
 
-        # create mock variable
-        if (is.null(prop_NA)) {
-            set.seed(seed)
-            col <- data.frame(new = sample(labels, length, replace = T)) 
-        } else { # optional: add NA levels
-            set.seed(seed)
-            vec <- sample(labels, length * (1-prop_NA), replace = T)
-            set.seed(seed)
-            vec.na <- sample(NA_labels, length * prop_NA, replace = T)
+            # create mock variable
+            if (is.null(prop_NA)) {
+                set.seed(seed)
+                col <- data.frame(new = sample(labels, length, replace = T)) 
+            } else { # optional: add NA levels
+                set.seed(seed)
+                vec <- sample(labels, length * (1-prop_NA), replace = T)
+                set.seed(seed)
+                vec.na <- sample(NA_labels, length * prop_NA, replace = T)
 
-            vec <- sample(c(vec, vec.na)) # combine and randomly sorted
-            col <- data.frame(new = c(vec, vec.na))[1:length] 
+                vec <- sample(c(vec, vec.na)) # combine and randomly sorted
+                col <- data.frame(new = c(vec, vec.na))[1:length] 
+            }
+            names(col)[1] <- var_raw
+            
+            return(col)
         }
-        names(col)[1] <- var_raw
-        
-        return(col)
-    }
+    }   
 }
 
 
--- R/create_con_var.R
+++ R/create_con_var.R
@@ -4,6 +4,7 @@
 #' @param cycle A string indicating the cycle (e.g., "CCHS2001").
 #' @param variable_details A data frame containing details about variables, including ranges and NA specifications.
 #' @param length An integer specifying the desired length of the generated variable.
+#' @param df_mock data frame, the current generated mock data, to check if the raw variable has already been created.
 #' @param type A string indicating the distribution type for the continuous variable. Can be "normal" or "uniform". Default is "normal".
 #' @param prop_NA A numeric value between 0 and 1, representing the proportion of NAs to introduce. If NULL, no NAs are introduced. Default is NULL.
 #' @param seed An integer for setting the random seed to ensure reproducibility. Default is 100.
@@ -15,47 +16,49 @@
 #' # create_con_var("income", "cycle2", variable_details, 500, type = "uniform", prop_NA = 0.1)
 #'
 
-create_con_var <- function(var, cycle, variable_details, length, 
+create_con_var <- function(var, cycle, variable_details, length, df_mock, 
   type = "normal", prop_NA = NULL, seed = 100) {
-    # related rows in variable_details
-    var_details <- variable_details[variable_details$variable == var & str_detect(variable_details$variableStart, cycle),]
+  # related rows in variable_details
+  var_details <- variable_details[variable_details$variable == var & str_detect(variable_details$variableStart, cycle),]
 
-    if (nrow(var_details) > 0) {
-        # extract the row variable name in the corresponding cycle
-        temp <- unlist(str_split(var_details$variableStart[1],","))
-        var_raw <- unlist(str_split(temp[str_detect(temp, cycle)],"::"))[2]
+  if (nrow(var_details) > 0) {
+      # extract the row variable name in the corresponding cycle
+      temp <- unlist(str_split(var_details$variableStart[1],","))
+      var_raw <- unlist(str_split(temp[str_detect(temp, cycle)],"::"))[2]
 
-        # extract values from `recStart` column
-        # real levels
-        rng <- var_details$recStart[!(str_detect(var_details$recEnd, "NA"))]
-        rng <- as.numeric(unlist(str_extract_all(rng, "\\d+\\.?\\d*"))) # extract integers or float
+    if (!(var_raw %in% names(df_mock))) {
+      # extract values from `recStart` column
+      # real levels
+      rng <- var_details$recStart[!(str_detect(var_details$recEnd, "NA"))]
+      rng <- as.numeric(unlist(str_extract_all(rng, "\\d+\\.?\\d*"))) # extract integers or float
         
-        # NA levels
-        NA_labels <- var_details$recStart[str_detect(var_details$recEnd, "NA")]
-        if (any(str_detect(NA_labels, ","))) { # unpack the ranges
-            temp <- NA_labels[!str_detect(NA_labels, ",")]
-            ranges <- NA_labels[str_detect(NA_labels, ",")]
-            for (range in ranges) {
-                temp <- c(temp, unpack_range(range))
-            }
-            NA_labels <- sort(temp)
-        }
+      # NA levels
+      NA_labels <- var_details$recStart[str_detect(var_details$recEnd, "NA")]
+      if (any(str_detect(NA_labels, ","))) { # unpack the ranges
+          temp <- NA_labels[!str_detect(NA_labels, ",")]
+          ranges <- NA_labels[str_detect(NA_labels, ",")]
+          for (range in ranges) {
+              temp <- c(temp, unpack_range(range))
+          }
+          NA_labels <- sort(temp)
+      }
 
-        # create mock variable
-        if (is.null(prop_NA)) {
-            col <- data.frame(new = create_vec(length, rng, type, seed)) 
-        } else { # optional: add NA levels
-            vec <- create_vec(length * (1-prop_NA), rng, type, seed)
-            set.seed(seed)
-            vec.na <- sample(NA_labels, length * prop_NA, replace = T)
+      # create mock variable
+      if (is.null(prop_NA)) {
+        col <- data.frame(new = create_vec(length, rng, type, seed)) 
+      } else { # optional: add NA levels
+        vec <- create_vec(length * (1-prop_NA), rng, type, seed)
+        set.seed(seed)
+        vec.na <- sample(NA_labels, length * prop_NA, replace = T)
 
-            vec <- sample(c(vec, vec.na)) # combine and randomly sorted
-            col <- data.frame(new = c(vec, vec.na))[1:length] 
-        }
-        names(col)[1] <- var_raw
+        vec <- sample(c(vec, vec.na)) # combine and randomly sorted
+        col <- data.frame(new = c(vec, vec.na))[1:length] 
+      }
+      names(col)[1] <- var_raw
       
-        return(col)
+      return(col)
     }
+  }
 }
 
 # ---------------------
